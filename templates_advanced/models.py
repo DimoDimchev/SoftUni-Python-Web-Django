f1d86aaa04d21d46e964c0601b80fa47e64a7716
Dimo Dimchev <ddimchev7@gmail.com>
2021-05-08 00:29:00 +0300

    Created custom template filter

--- templates_advanced/models.py
+++ templates_advanced/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Todo(models.Model):
     text = models.CharField(max_length=30, blank=False)
     is_done = models.BooleanField(default=False, blank=False)
+
+    def __str__(self):
+        return f'{self.text}; {self.is_done}'
