2a5c14b10d4d9afda0343f13e5aa377ab2063df2
Dimo Dimchev <ddimchev7@gmail.com>
2021-05-08 14:25:01 +0300

    Added image upload for 'Media Files' app

--- resources/models.py
+++ resources/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Pet(models.Model):
     name = models.CharField(max_length=30)
     image = models.ImageField(upload_to='pets')
+    passport = models.FileField(upload_to='documents')
 
     def __str__(self):
         return self.name
--- resources/views.py
+++ resources/views.py
@@ -11,16 +11,16 @@ def resources(request):
             'pets': Pet.objects.all(),
             'form': PetForm
         }
-        return render(request, 'resources/index.html', context)
+        return render(request, 'resources/pets.html', context)
     else:
         form = PetForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('pets')
+        else:
+            context = {
+                'pets': Pet.objects.all(),
+                'form': form,
+            }
 
-        context = {
-            'pets': Pet.objects.all(),
-            'form': form,
-        }
-
-        return render(request, 'resources/index.html', context)
+            return render(request, 'resources/pets.html', context)
