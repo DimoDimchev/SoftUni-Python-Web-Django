9f5931a92f85e69365a177071a15092532010e91
Dimo Dimchev <ddimchev7@gmail.com>
2021-05-14 21:29:28 +0300

    Implemented login view

--- auth_lecture/views.py
+++ auth_lecture/views.py
@@ -1,9 +1,16 @@
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
 from django.shortcuts import render, redirect
 
 
 def login_view(request):
-    pass
+    username = 'test_user'
+    password = 'testpwd123'
+    user = authenticate(username=username, password=password)
+    if user:
+        login(request, user)
+        return redirect('pets')
+
+    return redirect('pets')
 
 
 def logout_view(request):
