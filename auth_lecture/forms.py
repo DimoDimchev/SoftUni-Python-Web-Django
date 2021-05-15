dd53dcc9356dbb61f424f5f6cd89b990745d2252
Dimo Dimchev <ddimchev7@gmail.com>
2021-05-15 22:00:49 +0300

    Added form-based validation for email field

--- auth_lecture/forms.py
+++ auth_lecture/forms.py
@@ -6,3 +6,12 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'password', 'email')
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email', False)
+        if not email:
+            raise forms.ValidationError('Email is required')
+        return email
