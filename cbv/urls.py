f031a5fafdc1d53029d8500594f95de97da78aab
Dimo Dimchev <ddimchev7@gmail.com>
2021-05-17 22:12:30 +0300

    Implemented Template View

--- cbv/urls.py
+++ cbv/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from cbv.views import IndexView
+from cbv.views import IndexView, IndexTemplateView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='cbv index'),
+    path('2/', IndexTemplateView.as_view(), name='cbv index 2'),
 ]
--- cbv/views.py
+++ cbv/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.views.generic.base import View
+from django.views.generic.base import View, TemplateView
+
+from resources.models import Pet
 
 
 class IndexView(View):
     def get(self, request):
-        return render(request, 'cbv/index.html')
+        context = {
+            'heading_text': 'Hello from base View',
+            'pets': Pet.objects.all(),
+        }
+
+        return render(request, 'cbv/index.html', context)
 
     def post(self, request):
         pass
+
+
+class IndexTemplateView(TemplateView):
+    template_name = 'cbv/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['heading_text'] = 'Hello from template view'
+        context['pets'] = Pet.objects.all()
+
+        return context
