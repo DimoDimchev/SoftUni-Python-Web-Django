82d47cf9dff7e55d9faf528f55f7cc6de5922229
Dimo Dimchev <ddimchev7@gmail.com>
2021-05-21 15:53:53 +0300

    Added serializer for Book model

--- books_api/urls.py
+++ books_api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 
+from books_api.views import BooksAPIView
+
 urlpatterns = [
+    path('all/', BooksAPIView.as_view(), name='books list api'),
 ]
--- books_api/views.py
+++ books_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import views as rest_views
+from rest_framework.response import Response
 
-# Create your views here.
+from books_api.models import Book
+from books_api.serializers import BookSerializer
+
+
+class BooksAPIView(rest_views.APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
