91be6a77d1bc25b5035101b3d8df3d967571a2ce
Dimo Dimchev <ddimchev7@gmail.com>
2021-05-22 14:52:40 +0300

    Added generic views for Django REST framework

--- books_api/views.py
+++ books_api/views.py
@@ -1,32 +1,15 @@
-from django.shortcuts import render
-from rest_framework import views as rest_views, status
-from rest_framework.response import Response
+from rest_framework import generics
 
 from books_api.models import Book
 from books_api.serializers import BookSerializer
 
 
-class BooksListAPIView(rest_views.APIView):
-    def get(self, request):
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = BookSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class BooksListAPIView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 
-class BooksDetailsAPIView(rest_views.APIView):
-    def get(self, request, pk):
-        book = Book.objects.get(pk=pk)
-        serializer = BookSerializer(book, many=False)
-        return Response(serializer.data)
+class BooksDetailsAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
-    def delete(self, request, pk):
-        book = Book.objects.get(pk=pk)
-        book.delete()
-        return Response(status=status.HTTP_200_OK)
